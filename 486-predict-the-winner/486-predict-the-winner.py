825d1c6d64eb831ca10b5506f21ebb12e1534bd0
prayas99 <63349030+prayas99@users.noreply.github.com>
2022-09-08 12:38:05 +0530

    Time: 43 ms (83.69%), Space: 13.9 MB (85.16%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.py
+++ 486-predict-the-winner/486-predict-the-winner.py
@@ -1,15 +1,14 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
         n = len(nums)
-        @lru_cache(None)
-        def solve(i, j):
-            if i > j or i >= n or j < 0:
-                return 0
-            
-            option1 = nums[i] + min(solve(i+2, j), solve(i+1, j-1))
-            option2 = nums[j] + min(solve(i+1, j-1), solve(i, j-2))
-            
-            return max(option1, option2)
+        dp = [[0 for _ in range(n + 2)] for _ in range(n + 2)]
         
-        tom = solve(0, n - 1)
-        return tom >= sum(nums) / 2
+        for i in range(n - 1, -1, -1):
+            for j in range(2, n + 2):
+                if i > j - 2:
+                    continue
+                option1 = nums[i] + min(dp[i+2][j], dp[i+1][j-1])
+                option2 = nums[j - 2] + min(dp[i+1][j-1], dp[i][j-2])
+                dp[i][j] = max(option1, option2)
+                
+        return dp[0][n + 1] >= sum(nums) / 2
