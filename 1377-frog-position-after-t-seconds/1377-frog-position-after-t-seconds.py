722552339ed77ca528bd4cb0c19286a458843955
prayas99 <63349030+prayas99@users.noreply.github.com>
2022-09-11 19:53:16 +0530

    Time: 290 ms (5.14%), Space: 14 MB (81.82%) - LeetHub

---
+++ 1377-frog-position-after-t-seconds/1377-frog-position-after-t-seconds.py
@@ -0,0 +1,43 @@
+class Solution:
+    def frogPosition(self, n: int, edges: List[List[int]], t: int, target: int) -> float:
+        d = defaultdict(list)
+        for i, j in edges:
+            d[i].append(j)
+            d[j].append(i)
+            
+        def bfs():
+            layer, q = 0, deque([(1, 1)])
+            visited = set()
+            visited.add(1)
+            res = []
+            while q and layer < t:
+                for _ in range(len(q)):
+                    node, curr = q.popleft()  
+                    temp = 0
+                    for child in d[node]:
+                        if child not in visited:
+                            temp += 1       
+                    if temp == 0:
+                        q.append((node, curr))
+                    else:
+                        for child in d[node]:
+                            if layer == t - 1 and child not in visited and child == target:
+                                res.append(curr*temp)
+                            if layer < t - 1 and child not in visited:
+                                q.append((child, temp*curr))
+                                visited.add(child)
+                layer += 1
+            #print(res)
+            for val, time in q:
+                    if val == target:
+                        res.append(time)
+            #print(res)
+            ans = 1
+            if not res: return 0
+            for num in res:
+                ans *= (1/num)
+            return ans
+        return bfs()
+                
+        
+        
