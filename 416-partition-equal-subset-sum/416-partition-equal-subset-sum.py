24220da120ac587508b3ec4d7ab9eb3df0749ea8
prayas99 <63349030+prayas99@users.noreply.github.com>
2022-09-11 14:24:26 +0530

    Time: 1542 ms (57.04%), Space: 160.6 MB (7.39%) - LeetHub

---
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total = sum(nums)
+        if total % 2: return False
+        total //= 2
+        n = len(nums)
+        
+        @lru_cache(None)
+        def helper(i = 0, box1 = 0, box2 = 0):
+            if max(box1, box2) > total:
+                return
+            if i == n:
+                return box1 == box2
+            return helper(i + 1, box1 + nums[i], box2) or helper(i + 1, box1, box2 + nums[i])
+        
+        return helper()
+            
