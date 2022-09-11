e6c9316add4e307226bbf0a4d3e0444bc0723de7
prayas99 <63349030+prayas99@users.noreply.github.com>
2022-09-11 23:09:00 +0530

    Time: 1138 ms (5.07%), Space: 29.7 MB (72.57%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.py
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.py
@@ -1,25 +1,18 @@
 class Solution:
     def maxPerformance(self, n: int, speed: List[int], efficiency: List[int], k: int) -> int:
-        MOD = int(1e9) + 7
-        nums = sorted(zip(speed, efficiency), key = lambda x : x[1], reverse = True)
-        res = float('-inf')
-        h = []
-        curr_sum = 0
         
-        for i in range(k):
-            heapq.heappush(h, nums[i])
-            curr_sum += nums[i][0]
-            res = max(res, curr_sum * nums[i][1])
-            
-        for j in range(k, n):
-            sp, eff = heapq.heappop(h)
-            if nums[j][0] > sp:
-                heapq.heappush(h, nums[j])
-                curr_sum += (nums[j][0] - sp)
-                res = max(res, curr_sum * nums[j][1])
-            else:
-                heapq.heappush(h, (sp, eff))
-                
+        MOD = int(1e9) + 7
+        nums = sorted(zip(efficiency, speed), reverse = True)
+        h = []
+        curr_sum = res = 0
+        
+        for e, s in nums:
+            heapq.heappush(h, s)
+            curr_sum += s
+            if len(h) > k:
+                curr_sum -= heapq.heappop(h)
+            res = max(res, curr_sum * e)
+        
         return res % MOD
             
         
