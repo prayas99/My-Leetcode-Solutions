ef2fade5ce33d87aba2606a6af27523f6ea5e9e3
prayas99 <63349030+prayas99@users.noreply.github.com>
2022-09-12 12:05:21 +0530

    Time: 313 ms (14.86%), Space: 15.8 MB (16.06%) - LeetHub

--- 2086-minimum-number-of-buckets-required-to-collect-rainwater-from-houses/2086-minimum-number-of-buckets-required-to-collect-rainwater-from-houses.py
+++ 2086-minimum-number-of-buckets-required-to-collect-rainwater-from-houses/2086-minimum-number-of-buckets-required-to-collect-rainwater-from-houses.py
@@ -1,21 +1,17 @@
 class Solution:
     def minimumBuckets(self, street: str) -> int:
         street = [c for c in street]
-        street.insert(0, 'H')
         street.append('H')
         res = 0
-        for i in range(1, len(street) - 1):
+        for i in range(len(street) - 1):
             if street[i] == 'H':
                 if street[i + 1] == 'B' or street[i - 1] == 'B':
                     continue
                 if street[i + 1] == '.':
                     street[i + 1] = 'B'
                     res += 1
-                elif street[i - 1] == '.':
+                elif i > 0 and street[i - 1] == '.':
                     res += 1
                 else:
                     return -1
         return res
-        
-                    
-        
