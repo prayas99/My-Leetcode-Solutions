088043c893812bc9310244147a576c9909e42267
prayas99 <63349030+prayas99@users.noreply.github.com>
2022-09-18 20:49:02 +0530

    Time: 319 ms (12.10%), Space: 16 MB (44.51%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,34 +1,16 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        cur_max = height[0]
-        l_sum = 0
-        k = 0
-        tval = 0
-        for i,val in enumerate(height[1:]):
-            i+=1
-            if val>=cur_max:
-                width = (i-k)-1
-                tval+=(width*cur_max)-l_sum
-                k = i
-                l_sum = 0
-                cur_max = val
-            else:
-                l_sum+=val
-                
-        l_sum = 0
-        cur_max = height[-1]
-        j = len(height)-1
-        for i in range(len(height)-2, k-1, -1):
-            if height[i]>=cur_max:
-                width = (j-i)-1
-                tval+=(width*cur_max)-l_sum
-                j = i
-                l_sum = 0
-                cur_max = height[i]
-            else:
-                l_sum+=height[i]  
-                
-        return tval
-                
-                
+        n = len(height)
+        water = [0]*n
         
+        lmaxi = 0
+        for i in range(n):
+            lmaxi = max(lmaxi, height[i])
+            water[i] = lmaxi
+        
+        rmaxi = 0
+        for j in range(n - 1, -1, -1):
+            rmaxi = max(rmaxi, height[j])
+            water[j] = min(water[j], rmaxi) - height[j]
+        
+        return sum(water)
