9fe91c0636a4c705da9585aba2dbdcac9c11cd11
prayas99 <63349030+prayas99@users.noreply.github.com>
2022-09-20 15:04:48 +0530

    Time: 9050 ms (8.08%), Space: 37.8 MB (79.30%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
@@ -1,24 +1,12 @@
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
-        n, m = len(nums1), len(nums2)
-        # dp[i][j] means the length of repeated subarray of nums1[:i] and nums2[:j]
-        dp = [[0] * (m + 1) for _ in range(n + 1)]
+        m, n = len(nums1), len(nums2)
+        dp = [[0]*(n + 1) for _ in range(m + 1)]
+        # FAILING 5 TESTCASES WITH TLE, but using [0]*(n+1) goes better than 60%!!!!!!
         ans = 0
-        for i in range(1, n + 1):
-            for j in range(1, m + 1):
-                # if both character is same
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
                 if nums1[i - 1] == nums2[j - 1]:
-                    # then we add 1 to the previous state, which is dp[i - 1][j - 1]
-                    # in other word, we extend the repeated subarray by 1
-                    # e.g. a = [1], b = [1], length of repeated array is 1
-                    #      a = [1,2], b = [1,2], length of repeated array is the previous result + 1 = 2
                     dp[i][j] = dp[i - 1][j - 1] + 1
-                    # record the max ans here
                     ans = max(ans, dp[i][j])
-                # else:
-                    # if you are looking for longest common sequence,
-                    # then you put dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]); here
-                    # however, this problem is looking for subarray,
-                    # since both character is not equal, which means we need to break it here
-                    # hence, set dp[i][j] to 0
         return ans
