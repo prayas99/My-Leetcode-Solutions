e40a7127cf820f5a42f580be321f3d791aa1e772
prayas99 <63349030+prayas99@users.noreply.github.com>
2022-09-21 15:06:51 +0530

    Time: 495 ms (100.00%), Space: 20.5 MB (75.51%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.py
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.py
@@ -2,7 +2,7 @@ class Solution:
     def sumEvenAfterQueries(self, nums: List[int], queries: List[List[int]]) -> List[int]:
         sumi = sum([num for num in nums if num % 2 == 0])
         n, m = len(nums), len(queries)
-        ans = [0]*m
+        ans = []
         for i, (val, idx) in enumerate(queries):
             if nums[idx] % 2 == 0:
                 if val % 2 == 0:
@@ -12,7 +12,7 @@ class Solution:
             else:
                 if val % 2 != 0:
                     sumi += nums[idx] + val
-            ans[i] = sumi
+            ans.append(sumi)
             nums[idx] += val
         return ans
         
