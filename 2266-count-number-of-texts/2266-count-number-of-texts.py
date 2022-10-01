394fcef03ec4468d4ce8931bd312cdff0e156cb4
prayas99 <63349030+prayas99@users.noreply.github.com>
2022-10-01 15:25:38 +0530

    Time: 2716 ms (35.73%), Space: 242.6 MB (38.04%) - LeetHub

--- 2266-count-number-of-texts/2266-count-number-of-texts.py
+++ 2266-count-number-of-texts/2266-count-number-of-texts.py
@@ -1,11 +1,11 @@
 class Solution:
     def countTexts(self, s: str) -> int:
         n = len(s)
+        MOD = int(1e9) + 7
+        
         def limit(c):
-            if c == '7' or c=='9':
-                return 3
-            else:
-                return 2
+            return 3 if c == '7' or c=='9' else 2
+            
         @lru_cache(None)
         def helper(i = 0):
             if i >= n:
@@ -15,5 +15,6 @@ class Solution:
             while j < n and j - i <= limit(s[i]) and s[j] == s[i]:
                 res += helper(j + 1)
                 j += 1
-            return res % (int(1e9) + 7)
-        return helper() % (int(1e9) + 7)
+            return res % MOD
+        
+        return helper() % MOD
