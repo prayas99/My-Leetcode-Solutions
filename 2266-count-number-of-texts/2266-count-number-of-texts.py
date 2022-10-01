d5e03c3af395696c9e3d7502b8ca828908f9f482
prayas99 <63349030+prayas99@users.noreply.github.com>
2022-10-01 15:23:49 +0530

    Time: 4216 ms (18.15%), Space: 241.1 MB (39.19%) - LeetHub

--- 2266-count-number-of-texts/2266-count-number-of-texts.py
+++ 2266-count-number-of-texts/2266-count-number-of-texts.py
@@ -10,8 +10,8 @@ class Solution:
         def helper(i = 0):
             if i >= n:
                 return 1
-            j = i + 1            
-            res = helper(j)
+            j = i           
+            res = 0
             while j < n and j - i <= limit(s[i]) and s[j] == s[i]:
                 res += helper(j + 1)
                 j += 1
