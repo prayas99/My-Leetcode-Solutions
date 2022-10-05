f5d10a130a896559b0db82d281393433f2ae0142
prayas99 <63349030+prayas99@users.noreply.github.com>
2022-10-05 11:48:49 +0530

    Time: 7575 ms (44.22%), Space: 38.5 MB (28.57%) - LeetHub

--- 1690-stone-game-vii/1690-stone-game-vii.py
+++ 1690-stone-game-vii/1690-stone-game-vii.py
@@ -1,9 +1,31 @@
 class Solution:
-    def stoneGameVII(self, s: List[int]) -> int:
-        dp = [[0] * len(s) for _ in range(len(s))]
-        p_sum = [0] + list(accumulate(s))
-        for i in range(len(s) - 2, -1, -1):
-            for j in range(i + 1, len(s)):
-                dp[i][j] = max(p_sum[j + 1] - p_sum[i + 1] - dp[i + 1][j], 
-                               p_sum[j] - p_sum[i] - dp[i][j - 1]);
-        return dp[0][len(s) - 1]
+    def stoneGameVII(self, stones: List[int]) -> int:
+        n = len(stones)
+        dp = [[0 for _ in range(n + 2)] for _ in range(n + 2)]
+        
+        for i in range(n - 1, -1, -1):
+            for j in range(n):
+                
+                if i >= j:
+                    dp[i][j] = 0
+                
+                elif j - i == 1:
+                    dp[i][j] = max(stones[i], stones[j])
+                    
+                else:
+                    opt1 = min(stones[i + 1] + dp[i + 2][j], stones[j] + dp[i + 1][j - 1])  
+                    opt2 = min(stones[j - 1] + dp[i][j - 2], stones[i] + dp[i + 1][j - 1])
+                    dp[i][j] = max(opt1, opt2)
+                    
+        return dp[0][n - 1]
+    
+#         @lru_cache(None)
+#         def helper(i = 0, j = len(stones) - 1):
+#             if i >= j:
+#                 return 0
+            
+#             opt1 = min(stones[i + 1] + helper(i + 2, j), stones[j] + helper(i + 1, j - 1))
+#             opt2 = min(stones[j - 1] + helper(i, j - 2), stones[i] + helper(i + 1, j - 1))
+#             return max(opt1, opt2)
+#         return helper()
+        
